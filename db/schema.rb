5279cf251de70777c974ec288543a0ca6e47e501
Robin Dickson <robindickson@gmail.com>
2015-07-02 15:19:54 +0200

    Added comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702091942) do
+ActiveRecord::Schema.define(version: 20150702130237) do
+
+  create_table "comments", force: true do |t|
+    t.text    "body"
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: true do |t|
-    t.string "url"
-    t.string "title"
-    t.text   "description"
+    t.string  "url"
+    t.string  "title"
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
