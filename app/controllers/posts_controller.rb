eaa0b416d01608193df6b7ec64718d66adc20520
Robin Dickson <robindickson@gmail.com>
2015-07-05 15:23:56 +0200

    Added route for posts#new and posts#create with validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,22 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      flash[:notice] = "Your post was created."
+    else # in case of validation error
+      flash[:notice] = "There was an error with your post."
+      render 'new'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit!
   end
 
   def edit
