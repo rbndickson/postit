30ca041101664c1d280878082639686213bc3cd2
Robin Dickson <robindickson@gmail.com>
2015-07-06 11:55:23 +0200

    Improved categories route and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,5 @@
 class CategoriesController < ApplicationController
 
-  def index
-    @categories = Category.all
-  end
-
   def new
     @category = Category.new
   end
@@ -13,10 +9,14 @@ class CategoriesController < ApplicationController
 
     if @category.save
       flash[:notice] = "category added successfully"
-      redirect_to categories_path
+      redirect_to root_path
     else
       render :new
     end
+  end
+
+  def show
+    @category = Category.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, except: :destroy
-  resources :categories, only: [:index, :new, :create]
+  resources :categories, only: [:new, :create, :show]
 end
