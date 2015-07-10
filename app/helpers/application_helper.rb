ec7a46dce354d9f2f6734d57fdd4f4368a9e3488
Robin Dickson <robindickson@gmail.com>
2015-07-10 21:49:16 +0200

    Added helper to format date and time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
   def fix_url(str)
     str.starts_with?('http://') ? str : "http://#{str}"
   end
-  
+
+  def format_datetime(time)
+    time.to_formatted_s(:short)
+  end
+
 end
