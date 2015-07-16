255bf6ea8377da8709e2aa6723aa7669f5d903e9
Robin Dickson <robindickson@gmail.com>
2015-07-17 00:05:32 +0200

    Improved authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ PostitTemplate::Application.routes.draw do
   get '/register', to: 'users#new' # register_path
   get '/login', to: 'sessions#new' #login_path
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy' #logout_path
 
   resources :posts, except: :destroy do
     resources :comments, only: :create
