80be70ca7d488bb2465b9c294c787828304d2027
Robin Dickson <robindickson@gmail.com>
2015-07-17 15:08:40 +0200

    Added new user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(post_params)
     if @user.save
-      flash[:notice] = "New user created."
+      flash[:notice] = "You have been registered and logged in."
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       flash[:notice] = "Error"
