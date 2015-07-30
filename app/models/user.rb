57b0a1261e57d3009d11d9ac96669ad8bf72f267
Robin Dickson <robindickson@gmail.com>
2015-07-30 13:38:24 +0200

    Added slugs to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 3}
+
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def to_param
+    self.slug
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729122744) do
+ActiveRecord::Schema.define(version: 20150730111737) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150729122744) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "slug"
   end
 
   create_table "votes", force: true do |t|
