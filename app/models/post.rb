5cabfa091fbc18307d66284bd05683f128af3b44
Robin Dickson <robindickson@gmail.com>
2015-08-03 16:35:05 +0200

    Improved post slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,11 +23,35 @@ class Post < ActiveRecord::Base
     self.votes.where(vote: false).size
   end
 
-  def generate_slug
-    self.slug = self.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '') # removes all symbols except -
-  end
-
   def to_param
     self.slug
   end
+
+  def generate_slug
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug
+  end
+
+  def append_suffix(str, count)
+    if str.split('').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.downcase.strip
+    str.gsub!(' ', '-')
+    str.gsub!(/[^\w-]/, '') # removes all symbols except -
+    str
+  end
+
 end
